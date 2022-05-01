e478f251d10fc5e10219c6706d551e0f8ff3dfad
John <johnnyiceproductions@gmail.com>
2022-05-01 14:44:43 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,36 +1,36 @@
 // Login Data
-var twitch_channel_name = ""
+var twitch_channel_name = "Majijej"
 var streamlabs_token = ""
 var streamelements_token = ""
 var streamloots_token = ""
 
 // Initial Counter Config
-var initialHours = 5
+var initialHours = 2
 var initialMinutes = 0
 var initialSeconds = 0
 
 // General Twitch, Streamlabs And StreamElements Config
-var seconds_added_per_sub_prime = 30
-var seconds_added_per_sub_tier1 = 30
-var seconds_added_per_sub_tier2 = 60
-var seconds_added_per_sub_tier3 = 120
+var seconds_added_per_sub_prime = 60
+var seconds_added_per_sub_tier1 = 60
+var seconds_added_per_sub_tier2 = 180
+var seconds_added_per_sub_tier3 = 480
 
-var seconds_added_per_resub_prime = 30
-var seconds_added_per_resub_tier1 = 30
-var seconds_added_per_resub_tier2 = 60
-var seconds_added_per_resub_tier3 = 120
+var seconds_added_per_resub_prime = 60
+var seconds_added_per_resub_tier1 = 60
+var seconds_added_per_resub_tier2 = 180
+var seconds_added_per_resub_tier3 = 480
 
-var seconds_added_per_giftsub_tier1 = 30
-var seconds_added_per_giftsub_tier2 = 60
-var seconds_added_per_giftsub_tier3 = 120
+var seconds_added_per_giftsub_tier1 = 60
+var seconds_added_per_giftsub_tier2 = 180
+var seconds_added_per_giftsub_tier3 = 480
 
-var min_amount_of_bits = 500
-var seconds_added_per_bits = 30
+var min_amount_of_bits = 999999999999999
+var seconds_added_per_bits = 0
 
 // Streamlabs And StreamElements Config
-var min_donation_amount = 5
-var seconds_added_per_donation = 30
+var min_donation_amount = 999999999999999
+var seconds_added_per_donation = 0
 
 // Streamloots Config
-var min_amount_of_chests = 5
-var seconds_added_per_chests = 30
+var min_amount_of_chests = 99999999999999999
+var seconds_added_per_chests = 0
