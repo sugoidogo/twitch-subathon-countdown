8180cb482b115bc5a05ab32e87dff744639c3ebd
Danilo Titato <danilotitato@gmail.com>
2023-03-01 21:32:36 -0300

    Implement a skewed bell random for the added time in Random Hour

--- js/api/twitch.js
+++ js/api/twitch.js
@@ -12,9 +12,9 @@ if (twitch_channel_name !== "") {
 
     client.on('subscription', (channel, username, methods, message, userstate) => {
         if (!countdownEnded && subEnable) {
-            let factor_t1_local = (happy_hour_active ? factor_t1 : 1) * (random_hour_active ? randomInRangeNoRounding(...range_t1): 1);
-            let factor_t2_local = (happy_hour_active ? factor_t2  : 1) * (random_hour_active ? randomInRangeNoRounding(...range_t2): 1);
-            let factor_t3_local = (happy_hour_active ? factor_t3 : 1) * (random_hour_active ? randomInRangeNoRounding(...range_t3): 1);
+            let factor_t1_local = (happy_hour_active ? factor_t1 : 1) * (random_hour_active ? randomBellSkew(...range_t1): 1);
+            let factor_t2_local = (happy_hour_active ? factor_t2  : 1) * (random_hour_active ? randomBellSkew(...range_t2): 1);
+            let factor_t3_local = (happy_hour_active ? factor_t3 : 1) * (random_hour_active ? randomBellSkew(...range_t3): 1);
 
             switch (methods['plan']) {
                 case "Prime":
@@ -42,9 +42,9 @@ if (twitch_channel_name !== "") {
 
     client.on('resub', (channel, username, months, message, userstate, methods) => {
         if (!countdownEnded && subEnable) {
-            let factor_t1_local = (happy_hour_active ? factor_t1 : 1) * (random_hour_active ? randomInRangeNoRounding(...range_t1): 1);
-            let factor_t2_local = (happy_hour_active ? factor_t2  : 1) * (random_hour_active ? randomInRangeNoRounding(...range_t2): 1);
-            let factor_t3_local = (happy_hour_active ? factor_t3 : 1) * (random_hour_active ? randomInRangeNoRounding(...range_t3): 1);
+            let factor_t1_local = (happy_hour_active ? factor_t1 : 1) * (random_hour_active ? randomBellSkew(...range_t1): 1);
+            let factor_t2_local = (happy_hour_active ? factor_t2  : 1) * (random_hour_active ? randomBellSkew(...range_t2): 1);
+            let factor_t3_local = (happy_hour_active ? factor_t3 : 1) * (random_hour_active ? randomBellSkew(...range_t3): 1);
 
             switch (methods['plan']) {
                 case "Prime":
@@ -72,9 +72,9 @@ if (twitch_channel_name !== "") {
 
     client.on('subgift', (channel, username, months, recipient, methods, userstate) => {
         if (!countdownEnded && subEnable) {
-            let factor_t1_local = (happy_hour_active ? factor_t1 : 1) * (random_hour_active ? randomInRangeNoRounding(...range_t1): 1);
-            let factor_t2_local = (happy_hour_active ? factor_t2  : 1) * (random_hour_active ? randomInRangeNoRounding(...range_t2): 1);
-            let factor_t3_local = (happy_hour_active ? factor_t3 : 1) * (random_hour_active ? randomInRangeNoRounding(...range_t3): 1);
+            let factor_t1_local = (happy_hour_active ? factor_t1 : 1) * (random_hour_active ? randomBellSkew(...range_t1): 1);
+            let factor_t2_local = (happy_hour_active ? factor_t2  : 1) * (random_hour_active ? randomBellSkew(...range_t2): 1);
+            let factor_t3_local = (happy_hour_active ? factor_t3 : 1) * (random_hour_active ? randomBellSkew(...range_t3): 1);
 
             switch (methods['plan']) {
                 case "Prime":
@@ -102,7 +102,7 @@ if (twitch_channel_name !== "") {
 
     client.on('cheer', (channel, userstate, message) => {
         if (!countdownEnded && bitEnable) {
-            let factor_bits_local = (happy_hour_active ? factor_bits : 1) * (random_hour_active ? randomInRangeNoRounding(...range_bits): 1);
+            let factor_bits_local = (happy_hour_active ? factor_bits : 1) * (random_hour_active ? randomBellSkew(...range_bits): 1);
 
             if (userstate.bits >= min_amount_of_bits) {
                 let times = Math.floor(userstate.bits/min_amount_of_bits);
